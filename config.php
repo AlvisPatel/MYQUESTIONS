4afb45c4897a7c870cb303a4dea2cad681c5da3a
AlvisPatel <vadaliyaalvis@gmail.com>
2018-02-27 23:42:45 +0530

    nothing

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 	//$con=mysql_connect("localhost","root","") or die("Connection is not connect, please try aging");
 	//$selectDb = mysql_select_db("bank") or die("Database is not select");
 	@session_start();
+	@session_start() or die();
 	$con=mysqli_connect("localhost","root","","myquestions") or die("Connection is not connect, please try aging");
 
 ?>
