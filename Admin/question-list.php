3ab4c5b2a74f75015d7010be19fa69b70673be62
AlvisPatel <vadaliyaalvis@gmail.com>
2018-03-01 23:02:39 +0530

    inking path changes to get back formating of UI

--- Admin/question-list.php
+++ Admin/question-list.php
@@ -2,7 +2,7 @@
 
 <?php
 // session_start();
-include_once '../../config.php';
+include_once '../config.php';
 //
 // if(!isset($_SESSION['user']))
 // {
@@ -22,7 +22,7 @@ $res=mysqli_query($con, "SELECT q.q_id as q_id,p.u_id as u_id,q.q_title as q_tit
     <meta name="description" content="">
     <meta name="author" content="">
     <title>My Questions</title>
-    <link href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <link href="css/colors/blue.css" id="theme" rel="stylesheet">
 </head>
@@ -112,13 +112,13 @@ $res=mysqli_query($con, "SELECT q.q_id as q_id,p.u_id as u_id,q.q_title as q_tit
             </div>
         </div>
     </div>
-    <script src="../assets/plugins/jquery/jquery.min.js"></script>
-    <script src="../assets/plugins/bootstrap/js/tether.min.js"></script>
-    <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/plugins/jquery/jquery.min.js"></script>
+    <script src="assets/plugins/bootstrap/js/tether.min.js"></script>
+    <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
     <script src="js/jquery.slimscroll.js"></script>
     <script src="js/waves.js"></script>
     <script src="js/sidebarmenu.js"></script>
-    <script src="../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
     <script src="js/custom.min.js"></script>
 </body>
 
